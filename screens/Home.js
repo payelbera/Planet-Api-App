07fc16a90408bce9fcedb98bb4cf90788267e51f
payelbera <60781680+payelbera@users.noreply.github.com>
2023-02-20 08:40:58 +0530

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -13,43 +13,17 @@ import axios from "axios";
 export default class HomeScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      listData: [],
-      url: "http://localhost:5000/"
-    };
+    
   }
 
   componentDidMount() {
     this.getPlanets();
   }
 
-  getPlanets = () => {
-    const { url } = this.state;
-    axios
-      .get(url)
-      .then(response => {
-        return this.setState({
-          listData: response.data.data
-        });
-      })
-      .catch(error => {
-        Alert.alert(error.message);
-      });
-  };
+  
 
   renderItem = ({ item, index }) => (
-    <ListItem
-      key={index}
-      title={`Planet : ${item.name}`}
-      subtitle={`Distance from earth : ${item.distance_from_earth}`}
-      titleStyle={styles.title}
-      containerStyle={styles.listContainer}
-      bottomDivider
-      chevron
-      onPress={() =>
-        this.props.navigation.navigate("Details", { planet_name: item.name })
-      }
-    />
+    
   );
 
   keyExtractor = (item, index) => index.toString();
@@ -72,11 +46,7 @@ export default class HomeScreen extends Component {
           <Text style={styles.headerText}>Planets World</Text>
         </View>
         <View style={styles.lowerContainer}>
-          <FlatList
-            keyExtractor={this.keyExtractor}
-            data={this.state.listData}
-            renderItem={this.renderItem}
-          />
+          
         </View>
       </View>
     );
